034dfe093ae9cb89e6958656b648b88e07e1af55
Mike Voydanoff <voydanoff@google.com>
2017-05-08 09:16:12 -0700

    platform dev creating soc vid:pid:did devices
    Change-Id: I7c5063dc1b480ce9eab6ae076bd192b35de522dc

--- kernel/target/pc-x86/rules.mk
+++ kernel/target/pc-x86/rules.mk
@@ -24,5 +24,6 @@ MDI_SRCS := \
 
 MDI_INCLUDES := \
     kernel/include/mdi/kernel-defs.mdi \
+    system/ulib/ddk/include/mdi/platform-defs.mdi \
 
 include make/mdi.mk
--- system/udev/platform-bus/platform-bus.c
+++ system/udev/platform-bus/platform-bus.c
@@ -14,12 +14,95 @@
 #include <magenta/process.h>
 #include <magenta/syscalls.h>
 #include <mdi/mdi.h>
+#include <mdi/mdi-defs.h>
 
 // for now we are linked directly into the devhost
 extern mx_handle_t devhost_get_hmdi(void);
 
 
+typedef struct {
+    mx_device_t* mxdev;
+    mx_device_prop_t props[3];
+} platform_dev_t;
+
+static void platform_dev_release(void* ctx) {
+    platform_dev_t* dev = ctx;
+    free(dev);
+}
+
+static mx_protocol_device_t platform_dev_proto = {
+    .version = DEVICE_OPS_VERSION,
+    .release = platform_dev_release,
+};
+
+static void platform_bus_publish_devices(mdi_node_ref_t* bus_node, mx_driver_t* driver) {
+    mdi_node_ref_t  driver_node;
+    mdi_each_child(bus_node, &driver_node) {
+        if (mdi_id(&driver_node) != MDI_PLATFORM_BUS_DRIVER) {
+            printf("unexpected node %d in platform_bus_publish_devices\n", mdi_id(&driver_node));
+            continue;
+        }
+        uint32_t vid = 0;
+        uint32_t pid = 0;
+        uint32_t did = 0;
+        mdi_node_ref_t  node;
+        mdi_each_child(&driver_node, &node) {
+            switch (mdi_id(&node)) {
+                case MDI_PLATFORM_BUS_DRIVER_VID:
+                mdi_node_uint32(&node, &vid);
+                break;
+            case MDI_PLATFORM_BUS_DRIVER_PID:
+                mdi_node_uint32(&node, &pid);
+                break;
+            case MDI_PLATFORM_BUS_DRIVER_DID:
+                mdi_node_uint32(&node, &did);
+                break;
+            default:
+                break;
+            }
+
+            if (!vid || !pid || !did) {
+                printf("missing vid pid or did\n");
+                continue;
+            }
+
+            platform_dev_t* dev = calloc(1, sizeof(platform_dev_t));
+            if (!dev) return;
+
+            mx_device_prop_t props[] = {
+                {BIND_SOC_VID, 0, vid},
+                {BIND_SOC_PID, 0, pid},
+                {BIND_SOC_DID, 0, did},
+            };
+            static_assert(countof(props) == countof(dev->props), "");
+            memcpy(dev->props, props, sizeof(dev->props));
+
+            char name[50];
+            snprintf(name, sizeof(name), "pdev-%u:%u:%u\n", vid, pid, did);
+
+            device_add_args_t args = {
+                .version = DEVICE_ADD_ARGS_VERSION,
+                .name = name,
+                .ctx = dev,
+                .driver = driver,
+                .ops = &platform_dev_proto,
+                .proto_id = MX_PROTOCOL_SOC,
+                .props = dev->props,
+                .prop_count = countof(dev->props),
+            };
+
+            mx_status_t status = device_add(driver_get_root_device(), &args, &dev->mxdev);
+            if (status != NO_ERROR) {
+                printf("platform-bus failed to create device for %u:%u:%u\n", vid, pid, did);
+            } else {
+                printf("platform-bus added device %s\n", name);
+            }
+        }
+    }
+}
+
 static mx_status_t platform_bus_init(mx_driver_t* driver) {
+    printf("platform_bus_init\n");
 
     mx_handle_t mdi_handle = devhost_get_hmdi();
     if (mdi_handle == MX_HANDLE_INVALID) {
@@ -47,6 +130,12 @@ static mx_status_t platform_bus_init(mx_driver_t* driver) {
         goto fail;
     }
 
+    mdi_node_ref_t  bus_node;
+    if (mdi_find_node(&root_node, MDI_PLATFORM_BUS, &bus_node) != NO_ERROR) {
+        printf("platform_bus_init couldn't find MDI_PLATFORM_BUS\n");
+        goto fail;
+    }
+    platform_bus_publish_devices(&bus_node, driver);
     printf("platform_bus_init SUCCESS\n");
 
     return NO_ERROR;
